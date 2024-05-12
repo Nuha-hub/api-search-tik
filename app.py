ed53054a68b556b410145f120b4879b43d03695b
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-05-12 17:37:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,63 @@
-from flask import Flask, request, jsonify
-from user_agent import generate_user_agent
-import requests
-from uuid import uuid4
 from flask import Flask, jsonify, request
+import secrets,user_agent,requests,os
+from threading import Lock
 import requests
+from secrets import token_hex
+import uuid
+from user_agent import generate_user_agent
+from requests import post
+from requests import get
+import json
+
 
 app = Flask(__name__)
 
-@app.route('/check_email/<email>', methods=['GET'])
-def get_instagram_info(email):
-        user=email
-        he = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar,en;q=0.9',
-            'cookie': f'ig_did={uuid4()}; datr=8J8TZD9P4GjWjawQJMcnRdV_; mid=ZBOf_gALAAGhvjQbR29aVENHIE4Z; ig_nrcb=1; csrftoken=5DoPPeHPd4nUej9JiwCdkvwwmbmkDWpy; ds_user_id=56985317140; dpr=1.25',
-            'referer': f'https://www.instagram.com/{user}/?hl=ar',
-            'sec-ch-prefers-color-scheme': 'dark',
-            'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-            'sec-ch-ua-full-version-list': '"Chromium";v="112.0.5615.138", "Google Chrome";v="112.0.5615.138", "Not:A-Brand";v="99.0.0.0"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua-platform-version': '"10.0.0"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': generate_user_agent(),
-            'viewport-width': '1051',
-            'x-asbd-id': '198387',
-            'x-csrftoken': '5DoPPeHPd4nUej9JiwCdkvwwmbmkDWpy',
-            'x-ig-app-id': '936619743392459',
-            'x-ig-www-claim': '0',
-            'x-requested-with': 'XMLHttpRequest',
-        }
-
-        rr = requests.get(f'https://www.instagram.com/api/v1/users/web_profile_info/?username={user}', headers=he)
-        if 'data' in rr.text:
-            by = '@te9egram'
-            try:
-                Id = rr.json()['data']['user']['id']
-            except:
-                Id = ""
-            try:
-                name = rr.json()['data']['user']['full_name']
-            except:
-                name = ""
-            try:
-                bio = rr.json()['data']['user']['biography']
-            except:
-                bio = ""
-            try:
-                flos = rr.json()['data']['user']['edge_followed_by']['count']
-            except:
-                flos = ""
-            try:
-                flog = rr.json()['data']['user']['edge_follow']['count']
-            except:
-                flog = ""
-            try:
-                pr = rr.json()['data']['user']['is_private']
-            except:
-                pr = ""
-
-            try:
-                re = requests.get(f"https://o7aa.pythonanywhere.com/?id={Id}").json()
-                da = re['date']
-            except:
-                da = 'No Date'
-
-            return jsonify({
-                'name': name,
-                'user': user,
-                'followers': flos,
-                'following': flog,
-                'date': da,
-                'id': Id,
-                'bio': bio,
-                'private': pr,
-                'BY': by
-            })
-
-
-
+@app.route('/change_username/session=<sess>/username=<username>', methods=['GET'])
+def cohange(username,sess):
+	def getMe(sessionid):
+			re = requests.get('https://i.instagram.com/api/v1/accounts/current_user/?edit=true', headers={'User-Agent': 'Instagram 136.0.0.34.124 Android (23/6.0.1; 640dpi; 1440x2560; samsung; SM-G935; hero2lte; samsungexynos8890; en_US; 208061712)', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'x-ig-app-id': '936619743392459'}, cookies={"sessionid": (sessionid)},timeout=6).text
+			if 'message' in re: print('- error sessionid')
+			re = json.loads(re)
+			return re['user']['email'],re['user']['phone_number'],re['user']['full_name']
+	
+	
+	def change(username=str,email=str,sess=str,phone=str,name=str):
+		url = "https://www.instagram.com/api/v1/web/accounts/edit/"
+		payload = {
+		    "biography": f"Done claimd : {username}",
+		    "chaining_enabled": "",
+		    "email": f"{email}",
+		    "external_url": "",
+		    "first_name": f"{name}",
+		    "phone_number": phone,
+		    "username": f"{username}"
+		}	
+		headers = {
+		  'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
+		  'Content-Type': "application/x-www-form-urlencoded",
+		  'referer': "https://www.instagram.com/accounts/edit/",
+		  'x-requested-with': "XMLHttpRequest",
+		  'x-ig-app-id': "1217981644879628",
+		  'x-asbd-id': "129477",
+		  'x-csrftoken': "k1p6uVvQPg1AotcLDBA1j6ZcVy6Av21r",
+		  'origin': "https://www.instagram.com",
+		  'sec-fetch-dest': "empty",
+		  'sec-fetch-site': "same-origin",
+		  'x-instagram-ajax': "1013407291",
+		  'x-ig-www-claim': "hmac.AR1LQ3DDGLQ2vgXtK9fYa1v6YN-R3qDBG0fbdCTeV5lXVfg4",
+		  'accept-language': "ar",
+		  'sec-fetch-mode': "cors",
+		  'Cookie': f"csrftoken=k1p6uVvQPg1AotcLDBA1j6ZcVy6Av21r; ds_user_id=3274566085; rur=\"CLN\\0543274566085\\0541746837782:01f7fc53dde0e7a13a3db0ac87debc934ed959b1c2f8b4b2ee36a9800f5f2c997bcee2e1\"; sessionid={sess}; shbid=\"13497\\0543274566085\\0541746739892:01f73f1ca5762f89b744b61ee0dd9afd5fca002076bfa3d67dda11a987783bb188712a8e\"; shbts=\"1715203892\\0543274566085\\0541746739892:01f71d73c950f9380081ca4fe8c61198932b53dfa00ec8fdc685ad9109616c05bab7a956\"; dpr=2; ps_l=1; ps_n=1; ig_did=4AF63F23-BB45-45B2-A81B-B7F9158258E0; ig_nrcb=1; mid=ZNslaAAAAAECVYMQxCk4LEmZ3BAN; datr=ZCXbZGzqtYWhCkRH-23IiH9A"
+		}
+		
+		response = requests.post(url, data=payload, headers=headers)
+		print(response.text)
+		if '''{"status":"ok"}''' in response.text:
+			return ({'username':username,'status':'Done','By':'@te9egram'})
+		else:
+			return ({'username':username,'status':'bad','By':'@te9egram'})
+	email,phone,name=getMe(sess)
+	return change(email=email,sess=sess,username=username,phone=phone,name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
